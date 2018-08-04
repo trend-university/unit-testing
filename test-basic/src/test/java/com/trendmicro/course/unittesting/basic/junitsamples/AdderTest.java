9e96a9edfc561258ff356f53f2706ca907a8f2d1
codeworkscn <codeworkscn@gmail.com>
2018-08-04 21:28:20 +0800

    [basic] fix bad assert usage, use junit.Assert instead.

--- test-basic/src/test/java/com/trendmicro/course/unittesting/basic/junitsamples/AdderTest.java
+++ test-basic/src/test/java/com/trendmicro/course/unittesting/basic/junitsamples/AdderTest.java
@@ -1,5 +1,7 @@
 package com.trendmicro.course.unittesting.basic.junitsamples;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AdderTest {
@@ -7,49 +9,49 @@ public class AdderTest {
   @Test
   public void testSumPositiveNumbersOneAndOne() {
     Adder adder = new AdderImpl();
-    assert (adder.add(1, 1) == 2);
+    assertEquals(2, adder.add(1, 1));
   }
 
   // can it add the positive numbers 1 and 2?
   @Test
   public void testSumPositiveNumbersOneAndTwo() {
     Adder adder = new AdderImpl();
-    assert (adder.add(1, 2) == 3);
+    assertEquals(3, adder.add(1, 2));
   }
 
   // can it add the positive numbers 2 and 2?
   @Test
   public void testSumPositiveNumbersTwoAndTwo() {
     Adder adder = new AdderImpl();
-    assert (adder.add(2, 2) == 4);
+    assertEquals(4, adder.add(2, 2));
   }
 
   // is zero neutral?
   @Test
   public void testSumZeroNeutral() {
     Adder adder = new AdderImpl();
-    assert (adder.add(0, 0) == 0);
+    assertEquals(0, adder.add(0, 0));
   }
 
   // can it add the negative numbers -1 and -2?
   @Test
   public void testSumNegativeNumbers() {
     Adder adder = new AdderImpl();
-    assert (adder.add(-1, -2) == -3);
+    assertEquals(-3, adder.add(-1, -2));
   }
 
   // can it add a positive and a negative?
   @Test
   public void testSumPositiveAndNegative() {
     Adder adder = new AdderImpl();
-    assert (adder.add(-1, 1) == 0);
+    assertEquals(0, adder.add(-1, 1));
   }
 
   // how about larger numbers?
   @Test
   public void testSumLargeNumbers() {
     Adder adder = new AdderImpl();
-    assert (adder.add(1234, 988) == 2222);
+    assertEquals(2222, adder.add(1234, 988));
   }
 
 }
